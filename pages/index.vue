7d1b44327917cf2d5d7f52eb0ee81bd04fa36082
hondasports <hondasports@gmail.com>
2025-01-02 14:20:33 +0900

    文章を微調整

--- pages/index.vue
+++ pages/index.vue
@@ -136,7 +136,7 @@ const skills = ref([
   { title: 'Java', description: '業務系アプリのバックエンド開発。Oracle, MySQL, PostgreSQL を使用。J2EE を用いた API 構築で Guava や JUnit を活用。', useYear: 5 },
   { title: 'C, MFC', description: '解析アプリケーションの保守で使用。C#と接続するためにCRLを使ったりもしていた。', useYear: 3 },
   { title: 'PHP, Vue', description: 'フロントエンド開発の主担当（PHP:2割, Vue:8割）。直近は、新規プロダクトの設定画面構築に携わり、ライブラリ選定、設計、実装を担当。使用技術: ESLint, Prettier, Vue3, vee-validate, zod, ky, vitest', useYear: 6 },
-  { title: 'Perl', description: '半導体企業でのテキスト操作で使用。現在も特定のタスクで活用。', useYear: 5 },
+  { title: 'Perl', description: '半導体企業でのテキスト操作で使用。', useYear: 5 },
   { title: 'Python', description: 'Django を使った WebAPI プロトタイプ作成や、個人プロジェクト（Twitter Bot）で使用。', useYear: 1 },
 ]);
 
