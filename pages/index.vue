148471e816b27bcb6b8c209604412a38780f18cc
hondasports <hondasports@gmail.com>
2025-01-02 09:45:57 +0900

    微調整

--- pages/index.vue
+++ pages/index.vue
@@ -90,7 +90,7 @@
                   <div class="mt-4">
                     <Divider />
                     <p class="mb-2"><a href="http://applion.jp/iphone/app/633386996/" target="_blank">ニコカレ - ニコニコカレンダー</a></p>
-                    <p class="mb-2">友人の仕事がきっかけで作成したアプリ。メンテナンスをサボっていたため削除された。リンクは過去に残っていた紹介ページ。</p
+                    <p class="mb-2">友人の仕事がきっかけで作成したアプリ。メンテナンスをサボっていたため削除された。リンクは過去に残っていた紹介ページ。</p>
                     <Divider />
                     <p class="mb-2"><a href="https://x.com/Bun_chan_Bot/" target="_blank">Bun-chan-bot</a></p>
                     <p>ラズパイを使いたいというのと、日中部屋を留守にするのでモニタリングするために作成。</p>
